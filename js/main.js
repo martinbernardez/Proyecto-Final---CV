92e7304bab71b5d96dc074307abba5b2a8ade584
Martín Bernardez <martynb1971@hotmail.com>
2023-02-13 00:00:31 -0300

    Sacando punta al lápiz

--- js/main.js
+++ js/main.js
@@ -1,9 +1,8 @@
 /* Función: Compartir */
-document.getElementById('compartir').onclick = function nativeShare() {
+
+document.getElementById('share').onclick = function nativeShare() {
     if (navigator.share) {
         navigator.share({ title: "Diego Herrera", text: "Currículum Vitae", url: "https://www.ejemplo.com/" });
     }
     return false;
 }
-
-
